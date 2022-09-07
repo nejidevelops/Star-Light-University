9211d435fbaac28d8568dd7d4dba510e5dec77da
Newton Ombese <newtonombese1@gmail.com>
2022-09-07 11:28:11 +0300

    adding my navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar(){
+    return(
+        <div class="container-fluid">
+            <div className="navbar">
+                
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
