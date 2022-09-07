d4372ffa0405d83f5a933620421c1fc0f1dd028d
Newton Ombese <newtonombese1@gmail.com>
2022-09-07 12:36:40 +0300

    setting light mode dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
   return (
-    <div className="App">
-      <Routes>
-        <Route></Route>
-      </Routes>
+    <div className={darkMode ? 'dark-mode' : 'light-mode'}>
+      <div className='container'>
+        <span style={{color: darkMode ? "gray":"yellow"}}>☀︎</span>
+        <div className='switch-checkbox'>
+        <label className='switch'>
+          <input type="checkbox"
+            onChange={()=> setDarkMode(!darkMode)}
+          />
+          <span className='switch round'></span>
+        </label>  
+        
+        </div>      
+        <span style={{color: darkMode ? "yellow":"gray"}}>☽</span>        
+      </div>
     </div>
   );
 }
