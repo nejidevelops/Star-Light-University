3d45db7e261d869e6e11679f982a12aea6aa60a1
Newton Ombese <newtonombese1@gmail.com>
2022-09-09 06:19:11 +0300

    working on my POST method for the course application

--- src/CourseApplication.js
+++ src/CourseApplication.js
@@ -1,7 +1,42 @@
 import React from 'react'
 import './CourseApplication.css';
+import { useState } from 'react';
 
-function CourseApplication() {
+function CourseApplication({handlePosting}) {
+  const [courseData, setCourseData] = useState({
+    course_name: '',
+    fees: '',
+    department: '',
+    course_duration: '',
+  })
+
+  function handleSubmit(e){
+    e.preventDefault();
+    fetch(`https://group-3-backend-app.herokuapp.com/courses`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(courseData)
+    })
+    .then(r => r.json())
+    .then(data => {
+        handlePosting(data)
+    })
+
+    setCourseData({
+        course_name: '',
+        fees: '',
+        department: '',
+        course_duration: '',
+    })
+}
+
+  function handleChange(e){
+    setCourseData({
+        ...courseData, [e.target.name]: e.target.value,
+    });
+} 
   return (
     <div className="container">
       <div className="contact-box">
@@ -14,6 +49,8 @@ function CourseApplication() {
               required
               className ="field"
               placeholder='Course Name'
+              value={courseData.course_name}
+              onChange={handleChange}
             ></input>
             <br />
             <input
@@ -21,17 +58,23 @@ function CourseApplication() {
               required
               className='field'
               placeholder='Fees'
+              value={courseData.fees}
+              onChange={handleChange}
             ></input>
             <input
                type='text'
                placeholder="Department"
                className='field'
+               value={courseData.department}
+              onChange={handleChange}
             ></input>
            <input
                type='text'
                required
                className='field'
                placeholder='Course Duration in Months'
+               value={courseData.course_duration}
+              onChange={handleChange}
            ></input>
           <button className="btn2">Send</button>
           </form>
