3f9faec381113e1ccde1080077f3c446505e9078
Newton Ombese <newtonombese1@gmail.com>
2022-09-09 06:06:00 +0300

    working on my teacher's form

--- src/Teachers.js
+++ src/Teachers.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import './Teacher.css'
 
 function Teachers(){
   return (
-    <div>Teachers</div>
+    <div className="container">
+      <div className="contact-box">
+        <div className="left"></div>
+        <div className="right">
+          <h2>Enroll Now</h2>
+          <form>
+            <input
+              type='text'
+              required
+              className ="field"
+              placeholder='Teacher Name'
+            ></input>
+            <br />
+            <input
+              type='text'
+              required
+              className='field'
+              placeholder='Home Address'
+            ></input>
+            <input
+               type='text'
+               placeholder="Specialization"
+               className='field'
+            ></input>
+           <input
+               type='text'
+               required
+               className='field'
+               placeholder='Salary Received'
+           ></input>
+          <button className="btn2">Send</button>
+          </form>
+        </div>
+      </div>
+	  </div>
   )
 }
 
