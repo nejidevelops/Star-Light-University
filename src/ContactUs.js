99e06d63a8c6bc6366bcf21f87b1484c06c2be37
Newton Ombese <newtonombese1@gmail.com>
2022-09-09 16:15:49 +0300

    getting my homepage ready

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./ContactUs.css"
 
 function ContactUs(){
   return (
-    <div>ContactUs</div>
+    <div className="containerr">
+		<div className="contact-boxx">
+			<div className="lefty"></div>
+			<div className="righty">
+				<h2>Contact Us</h2>
+				<input type="text" className="field1" placeholder="Your Name" />
+				<input type="text" className="field1" placeholder="Your Email" />
+				<input type="text" className="field1" placeholder="Phone" />
+				<textarea placeholder="Message" className="field"></textarea>
+				<button className="btn1">Send</button>
+			</div>
+		</div>
+	</div>
   )
 }
 
