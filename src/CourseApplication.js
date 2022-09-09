1dbaa03a74fe3868bb845a9c91007d6d8ddea28c
Newton Ombese <newtonombese1@gmail.com>
2022-09-09 05:57:05 +0300

    working on my course form

--- src/CourseApplication.js
+++ src/CourseApplication.js
@@ -4,30 +4,41 @@ import React from 'react'
 
 function CourseApplication() {
   return (
-    <div>
-        <Container>
-            <h2>Apply For A Course</h2>
-            <Box>
-                <div>
-                    <FormControl>
-                        <InputLabel>Course Name</InputLabel>
-                        <input name='coursename' />
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Course Duration</InputLabel>
-                        <input name='courseperiod' />
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Fees</InputLabel>
-                        <input name='fees' />
-                    </FormControl>
-                </div>
-            </Box>
-                    <FormControl>
-                        <Button type="submit">Submit</Button>
-                    </FormControl>
-        </Container>
-    </div>
+    <div className="container">
+      <div className="contact-box">
+        <div className="left"></div>
+        <div className="right">
+          <h2>Enroll Now</h2>
+          <form>
+            <input
+              type='text'
+              required
+              className ="field"
+              placeholder='Course Name'
+            ></input>
+            <br />
+            <input
+              type='text'
+              required
+              className='field'
+              placeholder='Fees'
+            ></input>
+            <input
+               type='text'
+               placeholder="Department"
+               className='field'
+            ></input>
+           <input
+               type='text'
+               required
+               className='field'
+               placeholder='Course Duration in Months'
+           ></input>
+          <button className="btn2">Send</button>
+          </form>
+        </div>
+      </div>
+	  </div>
   )
 }
 
